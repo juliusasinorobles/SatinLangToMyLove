d3d04ebfcf52bf8e6dd1c1ca953a9de32d4bdcc1
julius <juiusasinorobles@yahoo.com>
2014-04-19 12:42:35 +0800

    First Drop
    First Repo Files

---
+++ underdogidols.com/application/views/register.php
@@ -0,0 +1,58 @@
+{content_header}
+
+<section class="main-content">
+  <div class="row">
+    <div class="medium-3 small-12 columns">        
+        <aside>
+        </aside>
+    </div>
+    <div class="medium-8 small-12 columns">      
+      <p>Kindly provide the informations below.</p>
+      <div class="contact-us_form-wrapper">
+        <form class="contact-us_form">
+          <div class="row">
+            <div class="large-6 columns">
+              <label>First Name
+              <input type="text" placeholder="" />
+              </label>
+              </div>
+            <div class="large-6 columns">
+              <label>Last Name
+              <input type="text" placeholder="" />
+              </label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="large-6 columns">
+              <label>Username
+              <input type="text" placeholder="" />
+              </label>
+            </div>
+            <div class="large-6 columns">
+              <label>Email Address
+              <input type="text" placeholder="" />
+              </label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="large-6 columns">
+              <label>Password
+              <input type="text" placeholder="" />
+              </label>
+            </div>
+            <div class="large-6 columns">
+              <label>Re-type Password
+              <input type="text" placeholder="" />
+              </label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="large-12 columns">
+              <a href="#" class="button [tiny small large] submit-button">Submit</a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
